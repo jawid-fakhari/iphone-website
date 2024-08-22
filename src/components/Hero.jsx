464fbe9b85895b444ae975ceb55045908b278b73
jawid fakhari <jawid_fakhari@live.it>
2024-08-22 17:20:41 +0200

    highlights component completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,21 +18,30 @@ const Hero = () => {
   // useEffect che usa resize listener, quindi chiama funzione handleVideo... quando larghezza dello schermo si cambia
   useEffect(() => {
     window.addEventListener("resize", handleVideoSrcSet);
+
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
   }, []);
-  
+
   useGSAP(() => {
     gsap.to("#hero", {
       opacity: 1,
       delay: 1.5,
     });
+    gsap.to("#cta", {
+      opacity: 1,
+      y: -50,
+      delay: 2,
+    });
   }, []);
   return (
     <section className="w-full nav-height bg-black relative">
-      <div className="h-5/6 w-full flex-center flex-col">
+      <div className="h-auto w-full flex-center flex-col">
         <p id="hero" className="hero-title">
           iPhon 17 Pro
         </p>
-        <div className="md: w-10/12 w-9/12">
+        <div className="md:w-10/12 w-9/12">
           <video
             className="pointer-events-none"
             autoPlay
@@ -43,6 +52,15 @@ const Hero = () => {
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
+      </div>
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p>From $199/month or $999</p>
       </div>
     </section>
   );
